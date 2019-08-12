529f95bbf899d9530ba596288dd7c346e09b19f8
Joao Neto <51549485+joaocaneto@users.noreply.github.com>
2019-08-11 22:28:49 -0300

    https

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+
+class HttpsProtocol
+{
+
+    public function handle($request, Closure $next)
+    {
+        return redirect()->secure($request->getRequestUri());
+    }
+}
