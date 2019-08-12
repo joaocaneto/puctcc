625594e52c4f2365ff9ff22219f25836238f802c
Joao Neto <51549485+joaocaneto@users.noreply.github.com>
2019-08-11 21:46:07 -0300

    Revert "Update https->http"
    This reverts commit 6b968b5caf4d0c883e5cb041175918b8cdfe569d.

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://puctcc.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://puctcc.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
